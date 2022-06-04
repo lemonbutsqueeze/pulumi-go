00aebc5ceecd2c689b21df5610e59c8280a18f40
rafiqirosli <fricheps@Hotmail.com>
2022-06-04 21:35:17 +0930

    basic vpc interim

--- network/go.mod
+++ network/go.mod
@@ -3,6 +3,7 @@ module pulumi-go
 go 1.16
 
 require (
+	github.com/aws/aws-sdk-go-v2/config v1.15.9
 	github.com/pulumi/pulumi-aws/sdk/v5 v5.1.0
 	github.com/pulumi/pulumi/sdk/v3 v3.28.0
 )
--- network/main.go
+++ network/main.go
@@ -1,20 +1,54 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/s3"
+
+	// "github.com/aws/aws-sdk-go-v2/config"
+	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/ec2"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
+// func authenticateAws() {
+// 	cfg, err := config.LoadDefaultConfig(context.TODO())
+
+// 	if err != nil {
+// 		log.Fatalf("Failed to load configuration, %v", err)
+// 	}
+// }
+
+func createVpc(ctx *pulumi.Context) (*ec2.Vpc, error) {
+	vpc, err := ec2.NewVpc(ctx, "ApplicationVpc", &ec2.VpcArgs{
+		CidrBlock:        pulumi.String("10.0.0.0/16"),
+		EnableDnsSupport: pulumi.Bool(true),
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return vpc, err
+}
+
+// func createSubnets(ctx *pulumi.Context, vpc *ec2.Vpc) {
+// 	ec2.NewSubnet(ctx, "PrivateSubnet", &ec2.SubnetArgs{
+// 		VpcId: pulumi.Any(vpc.),
+// 		CidrBlock: pulumi.String("10.0.0.0/16"),
+// 	})
+// }
+
 func main() {
+	// authenticateAws()
+	println("Hello this got run first")
+
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		// Create an AWS resource (S3 Bucket)
-		bucket, err := s3.NewBucket(ctx, "my-bucket", nil)
+
+		vpc, err := createVpc(ctx)
+
 		if err != nil {
 			return err
 		}
 
-		// Export the name of the bucket
-		ctx.Export("bucketName", bucket.ID())
+		println(vpc)
+
 		return nil
 	})
 }
